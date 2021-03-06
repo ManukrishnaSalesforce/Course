bfd540adc30e3213cc0ca0f92f78f1109602c757
ManukrishnaSalesforce <79987101+ManukrishnaSalesforce@users.noreply.github.com>
2021-03-06 13:28:34 +0530

    Delete Dev directory

--- Dev/EXFiles/data/DeleteAllScratchOrgs.sh
+++
@@ -1,7 +0,0 @@
-echo "Please do not use this in a real production org, because you may lose work :-)"
-echo "Exporting scratch orgs..."
-sfdx force:data:soql:query -q "SELECT Id, Username FROM ScratchOrgInfo" -r csv -u dhLWCCourse  > ./ScratchOrgs.csv
-cat ./ScratchOrgs.csv
-echo "Deleting found scratch Orgs..."
-sfdx force:data:bulk:delete -f ./ScratchOrgs.csv -s ScratchOrgInfo -w 10 -u dhLWCCourse
-rm -f ./ScratchOrgs.csv
--- Dev/force-app/Base/main/default/triggers/CourseTrigger.trigger
+++
@@ -1,5 +0,0 @@
-trigger CourseTrigger on Course__c (after update) {
-	if (Trigger.isAfter && Trigger.isUpdate) {
-		CourseTriggerHandler.checkStatus(Trigger.new, Trigger.oldMap, Trigger.newMap);
-	}
-}
